70e1d325502d7822ec7eef27b1dd802dc176b7c3
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-04 01:12:53 +0300

    Update the-time-in-words.cs

--- the-time-in-words.cs
+++ the-time-in-words.cs
@@ -26,7 +26,41 @@ class Result
 
     public static string timeInWords(int h, int m)
     {
+        string[] hourWords = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
         
+        string[] minuteWords = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+                                    , "eleven", "twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"
+                                    , "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight","twenty nine" };
+
+        if (m == 0)
+        {
+            return $"{hourWords[h - 1]} o' clock";
+        }
+        if ((m > 0 && m < 15) || (m > 15 && m < 30))
+        {
+            return $"{minuteWords[m - 1]} minutes past {hourWords[h - 1]}";
+        }
+
+        if ((m > 30 && m < 45) || (m > 45 && m < 60))
+        {
+          return $"{minuteWords[60 - m - 1]} minutes to {hourWords[h]}";
+        }
+
+        if (m == 15)
+        {
+           return $"quarter past {hourWords[h - 1]}";
+        }
+
+        if (m == 30)
+        {
+           return $"half past {hourWords[h - 1]}";
+        }
+
+        if (m == 45)
+        {
+            return $"quarter to {hourWords[h]}";
+        }
+        return string.Empty;
     }
 
 }
